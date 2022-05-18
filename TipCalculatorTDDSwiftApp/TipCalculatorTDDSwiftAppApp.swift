ca472027a478fc6993bcf6bfdceefcb68518339d
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-18 22:58:32 +0545

    Initial Commit

---
+++ TipCalculatorTDDSwiftApp/TipCalculatorTDDSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTDDSwiftAppApp.swift
+//  TipCalculatorTDDSwiftApp
+//
+//  Created by Bikram Aryal on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorTDDSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
