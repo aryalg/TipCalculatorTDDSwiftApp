9a737a6346dcd0a8298f7375ee96ad4151fb12c7
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-19 00:25:35 +0545

    Refactor

--- TipCalculatorTDDSwiftApp/ContentView.swift
+++ TipCalculatorTDDSwiftApp/ContentView.swift
@@ -34,6 +34,7 @@ struct ContentView: View {
                 } label: {
                     EmptyView()
                 }.pickerStyle(.segmented)
+                    .accessibilityIdentifier("tipPercentageSegmentedControl")
                 
                 
                 Button("Calculate Tip") {
--- TipCalculatorTDDSwiftAppUITests/TipCalculatorTDDSwiftAppUITests.swift
+++ TipCalculatorTDDSwiftAppUITests/TipCalculatorTDDSwiftAppUITests.swift
@@ -9,15 +9,36 @@ import XCTest
 
 class when_content_view_is_shown: XCTestCase {
     
-    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
+    private var app: XCUIApplication!
+    
+    
+    override func setUp() {
         
-        let app = XCUIApplication()
+        app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
         
-       let totalTextField = app.textFields["totalTextField"]
+    }
+    
+    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
+        
+        let totalTextField = app.textFields["totalTextField"]
         
         XCTAssertEqual(totalTextField .value as! String, "Enter total")
     }
-   
+    
+    func test_should_make_sure_the_20_percent_default_tip_option_is_selected() {
+        
+        let tipPercentageSegmentedControl = app.segmentedControls["tipPercentageSegmentedControl"]
+        
+        let segmentedControlButton = tipPercentageSegmentedControl.buttons.element(boundBy: 1)
+        
+        XCTAssertEqual(segmentedControlButton.label, "20%")
+        XCTAssertTrue(segmentedControlButton.isSelected)
+    }
+    
+    override func tearDown() {
+        // clean up 
+    }
+    
 }
