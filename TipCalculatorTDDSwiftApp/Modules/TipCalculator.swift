188f8f5386bfb7a17a2ab6323812f1e2f3020da2
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-18 23:26:24 +0545

    Implementing Test to Calculate Tip

---
+++ TipCalculatorTDDSwiftApp/Modules/TipCalculator.swift
@@ -0,0 +1,25 @@
+//
+//  TipCalculator.swift
+//  TipCalculatorTDDSwiftApp
+//
+//  Created by Bikram Aryal on 18/05/2022.
+//
+
+import Foundation
+
+
+enum TipCalculatorError: Error {
+    case invalidInput
+}
+
+class TipCalculator {
+    
+    func calculate(total: Double, tipPercentage: Double) throws -> Double {
+        
+        if total < 0 {
+            throw TipCalculatorError.invalidInput
+        }
+        return total * tipPercentage;
+    }
+    
+}
